1b897fa7f194a5f72cf414e372492d199428e5b7
Shubham DDevOps <shubhamddevops@gmail.com>
2021-02-01 17:42:21 +0530

    added destructured props syntax

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,23 +1,23 @@
 /*
 Drop Down Component to show the dymanic content in dropdown.
-Props Needed : 
-        1. pass {content} prop in the form of an array (JSON array)
-        2. pass {name} prop in the form of a string
-        3. pass {id} prop in the form of a string
+Props Needed in below sequence:
+        1. pass {name} prop in the form of a string
+        2. pass {id} prop in the form of a string 
+        3. pass {content} prop in the form of an array (JSON array)      
 */
 import React from "react";
 import PropTypes from "prop-types";
 import "../scss/DropDown.scss";
-const DropDown = (props) => {
+const DropDown = ({ name, id, content }) => {
   return (
-    <div className="drop-down" id={props.id}>
-      <button className="drop-down-btn" id={props.id + "-btn"}>
-        {props.name}
+    <div className="drop-down" id={id}>
+      <button className="drop-down-btn" id={id + "-btn"}>
+        {name}
       </button>
-      <div className="drop-down-content" id={props.id + "-content"}>
-        {props.content.map((value, index) => (
+      <div className="drop-down-content" id={id + "-content"}>
+        {content.map((value, index) => (
           // eslint-disable-next-line
-          <a key={props.id + value + index}>{value}</a>
+          <a key={id + value + index}>{value}</a>
         ))}
       </div>
     </div>
